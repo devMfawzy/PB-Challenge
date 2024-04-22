11c79104f69f18d1f1ca4ff7c99fc248aac3c042
ww-mfawzy <mohamed.fawzy@ww.com>
2024-04-22 19:24:33 +0200

    initial commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Mohamed Fawzy on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
