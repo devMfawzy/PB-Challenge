3462dd101de925a7cea4fd2ecafdf0d7e90a9503
ww-mfawzy <mohamed.fawzy@ww.com>
2024-05-01 21:09:17 +0300

    refactored TransactionListViewModel

--- WorldOfPAYBACK/WorldOfPAYBACK/View/Transactions/TransactionListViewModel.swift
+++ WorldOfPAYBACK/WorldOfPAYBACK/View/Transactions/TransactionListViewModel.swift
@@ -9,52 +9,71 @@ import Foundation
 
 @Observable
 final class TransactionListViewModel {
-    // MARK: - Dependencies
+    // MARK: - Observation Ignored
     @ObservationIgnored private(set) var service: TransactionsServiceProtocol
-    @ObservationIgnored private(set) var settings: UserSettings
+    @ObservationIgnored private(set) var settings: UserSettingsProtocol
+    @ObservationIgnored private(set) var currentTask: Task<(), Error>?
     
     // MARK: - Observation tracked
-    private(set) var loadState = LoadState.idle
+    private(set) var allTransactions = [TransactionItem]()
     private(set) var isLoading = false
+    private(set) var failureMessage: String?
     var isFilterViewPresented = false
-    
-    // MARK: - Init
-    init(service: TransactionsServiceProtocol = TransactionsService(),
-         settings: UserSettings = UserSettings()) {
-        self.service = service
-        self.settings = settings
-    }
-    
-    // MARK: - Properies
-    @ObservationIgnored private(set) var currentTask: Task<(), Error>?
-    @ObservationIgnored private(set) var selectedCategory: Category?
-    @ObservationIgnored private(set) var allTransactions = [TransactionItem]()
-    var filteredTransactions: [TransactionItem] {
+    private(set) var selectedCategory: Category?
+    var transactions: [TransactionItem] {
         guard let selectedCategory else {
             return allTransactions
         }
         return allTransactions.filter { Category(id: $0.category) == selectedCategory }
     }
-    
     var sumOfTransactions: TransactionItem.TransactionDetail.Value {
-        let sum = filteredTransactions.map( { $0.transactionDetail.value.amount } ).reduce(0, +)
-        let currency = filteredTransactions.map( { $0.transactionDetail.value.currency } ).first ?? ""
+        let sum = transactions.map( { $0.transactionDetail.value.amount } ).reduce(0, +)
+        let currency = transactions.map( { $0.transactionDetail.value.currency } ).first ?? ""
         return .init(amount: sum, currency: currency)
     }
-    
     var categories: [Category] {
         let categorieIdsSet = Set<Int>(allTransactions
             .map { $0.category })
         return categorieIdsSet.sorted().map { Category(id: $0) }
     }
+    var shouldShowFliterView: Bool {
+        categories.count > 1
+    }
     
+    // MARK: - Init
+    init(service: TransactionsServiceProtocol = TransactionsService(),
+         settings: UserSettingsProtocol = UserSettings()) {
+        self.service = service
+        self.settings = settings
+    }
+        
     // MARK: - Methods
-    func loadTransactions(showIndicator: Bool = true) {
+    func dispatchAction(action: Action) {
+        switch action {
+        case .viewWillAppear:
+            if allTransactions.isEmpty
+                || settings.networkEnvironment != service.environment {
+                service.environment = settings.networkEnvironment
+                loadTransactions()
+            }
+        case .showFilterView:
+            isFilterViewPresented = true
+        case .didResetTransactionsFilter:
+            selectedCategory = nil
+        case .didPullToRefresh:
+            loadTransactions(showIndicator: false)
+        case .didTapRetry:
+            failureMessage = nil
+            loadTransactions()
+        case .didSelectCategory(let category):
+            selectedCategory = category
+        }
+    }
+    
+    private func loadTransactions(showIndicator: Bool = true) {
         currentTask?.cancel()
         isLoading = showIndicator
-        if case .failure = loadState {
-            loadState = .idle
-        }
+        failureMessage = nil
         currentTask = Task {
             do {
                 let transactions = try await service.getTransactions().sortedByDate
@@ -65,41 +84,28 @@ final class TransactionListViewModel {
         }
     }
     
-    func reloadTransactionsOnNetworkChange() {
-        if settings.networkEnvironment != service.environment {
-            service.environment = settings.networkEnvironment
-            loadTransactions()
-        }
-    }
-    
-    func filterTransactions(by category: Category) {
-        selectedCategory = category
-        loadState = .transactions(filteredTransactions)
-    }
-    
-    func resetTransactionsFilter() {
-        selectedCategory = nil
-        loadState = .transactions(filteredTransactions)
-    }
-    
     @MainActor
     private func handleSuccess(transactions: [TransactionItem]) {
         isLoading = false
-        selectedCategory = nil
         allTransactions = transactions
-        loadState = .transactions(transactions)
+        selectedCategory = nil
     }
     
     @MainActor
     private func handleError(_ error: Error) {
         isLoading = false
-        loadState = .failure(error.localizedDescription)
+        failureMessage = error.localizedDescription
+        allTransactions.removeAll()
     }
     
     // MARK: - Iner Type
-    enum LoadState: Equatable {
-        case idle
-        case failure(String)
-        case transactions([TransactionItem])
+    
+    enum Action {
+        case viewWillAppear
+        case showFilterView
+        case didResetTransactionsFilter
+        case didPullToRefresh
+        case didTapRetry
+        case didSelectCategory(category: Category?)
     }
 }
